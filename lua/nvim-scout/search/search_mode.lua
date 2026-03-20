1f7bbdbcfbc331cf51fbed4dcec9f64ad21d401d
Eric Spidle <ericspidledev@gmail.com>
2026-03-20 15:57:22 -0400

    search_mode: update banner config to update in place

--- lua/nvim-scout/search/search_mode.lua
+++ lua/nvim-scout/search/search_mode.lua
@@ -31,7 +31,6 @@ function scout_search_mode:update_banner_config(border, display_col, search_bar_
     self.banner_config["col"] = display_col
     self.banner_config["win"] = search_bar_window
     self.display_col = display_col
-    return self.banner_config
 end
 
 function scout_search_mode:get_banner_display_width()
