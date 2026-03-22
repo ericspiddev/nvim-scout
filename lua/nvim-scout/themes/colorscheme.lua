655471d48cf37a121ceee619059e55778509eca9
Eric Spidle <ericspidledev@gmail.com>
2026-03-22 10:56:29 -0400

    colorschemes: add tests for registering schemes

--- lua/nvim-scout/themes/colorscheme.lua
+++ lua/nvim-scout/themes/colorscheme.lua
@@ -13,22 +13,20 @@ function scout_colorscheme:init(namespace)
 end
 
 function scout_colorscheme:register_colorscheme(name, colorscheme)
-    local new_scheme = {}
+    local global_mark = "scout_g"
     if not self.namespace then
         Scout_Logger:error_print("Failed to register colorscheme because of nil namespace ", name)
         return
     end
     for _, key in pairs(Scout_Consts.colorscheme_groups) do
         if colorscheme[key] == nil then
-            Scout_Logger:warning_print("Missing key " .. key .. "for colorsheme ", colorscheme)
-        elseif key:find("scout_g") then
+            Scout_Logger:warning_print("Missing key " .. key .. " for colorscheme ", name)
+        elseif key:find(global_mark) then
             vim.api.nvim_set_hl(0, key, colorscheme[key]) -- make available for global use
         else
             vim.api.nvim_set_hl(self.namespace, key, colorscheme[key])
-            new_scheme[key] = colorscheme[key]
         end
     end
-    return new_scheme
 end
 
 
